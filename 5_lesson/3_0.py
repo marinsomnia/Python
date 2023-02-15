7d843a01e5ff4a4c77cd0f31145aba56251c3609
Mariia <utkaeva.maria@yandex.ru>
2023-02-15 15:57:32 +0300

    Task 3_0

---
+++ 5_lesson/3_0.py
@@ -0,0 +1,16 @@
+# 35.Напишите функцию, которая принимает одно число и проверяет, является ли оно простым
+# Напоминание: Простое число - это число, которое имеет 2 делителя: 1 и n(само число)
+# Input: 5 Output: yes
+
+def is_prime_number(n):
+    if n == 2:
+        return True
+    elif n == 1 or n % 2 == 0:
+        return False
+    for i in range(3, int((n ** 0.5) + 1), 2):
+        if (n % i == 0):
+            return False
+    return True
+
+
+print(is_prime_number(int(input())))
