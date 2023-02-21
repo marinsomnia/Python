22e1211f8a5690ee4e34caa3e78bbbe51a0ca0dc
Mariia <utkaeva.maria@yandex.ru>
2023-02-21 12:46:02 +0300

    Task 1_0

---
+++ 7_lesson/1_0.py
@@ -0,0 +1,9 @@
+# Напишите такое лямбда-выражение transformation, чтобы transformed_values получился копией values.
+
+values = [1, 23, 42, 'asdfg']
+transformation = lambda x: x
+transformed_values = list(map(transformation, values))
+if values == transformed_values:
+    print('ok')
+else:
+    print('fail')
